5b30b2f41c9f497c1914fa78f9a010e6679058a5
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-08-27 19:48:53 +0700

    Add project files.

---
+++ SK-Lib-cmath-isgreaterequal-Bagian5__CPP/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+/*
+	Source by GeeksForGeeks
+	Modified For Learn by RK
+	I.D.E : VS2019
+*/
+
+
+using namespace std;
+
+int main() {
+
+	for (int i = 1; isgreaterequal(10, i); i++) {
+
+		cout << "12 x " << i << " = " << 12 * i << endl;
+	}
+
+	_getch();
+	return 0;
+}
